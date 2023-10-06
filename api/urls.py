6101c2b77bb4896060df3d5a0e477d91462bb94c
bloodlean <bloodlean7@gmail.com>
2023-10-06 17:22:08 +0300

    commit4

--- api/urls.py
+++ api/urls.py
@@ -3,11 +3,11 @@ from .views import *
 
 urlpatterns = [
     path('user/', user),
-    path('user/<int:pk>/', user),
+    path('user/<int:pk>/', user_detail),
     path('project/', project),
-    path('project/<int:pk>/', project),
+    path('project/<int:pk>/', project_detail),
     path('task/', task),
-    path('task/<int:pk>/', task),
+    path('task/<int:pk>/', task_detail),
 
     path('auth/', include('dj_rest_auth.urls'))
 ]
