291a5b0588929d5ff36c617e78b30b1f1b65448d
bloodlean <bloodlean7@gmail.com>
2023-10-08 12:04:40 +0300

    commit13

--- app/models.py
+++ app/models.py
@@ -29,7 +29,7 @@ class Task(models.Model):
     title = models.CharField('Название задачи', max_length=256)
     description = models.TextField('Описание задачи', blank=True, null=True)
     deadline_date = models.DateField('Крайний срока', blank=True, null=True)
-    project = models.ForeignKey(Project, on_delete=models.CASCADE, verbose_name='Проект')
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, blank=True, null=True, verbose_name='Проект')
     assigned_to = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, verbose_name='Назначен на')   
     completed = models.BooleanField('Завершен', default=False)
 
